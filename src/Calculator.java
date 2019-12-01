d846ff406b8bfd0c82bcb55f40161a90debd8a19
leanlynn <lynette.oh96@hotmail.com>
2019-12-01 21:32:09 +0800

    added comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,36 +4,54 @@ import java.util.Stack;
 public class Calculator {
 
 	public static double calculate(String sum) {
+		
 		if( sum == null || sum.length() == 0 ) return 0;
 		
 		//temporary store result if encounter brackets
 		Stack<Double> results = new Stack<Double>();
 		
 		double result = 0;
+
+		// last seen sign
 		char sign = '+';
 		boolean isDecimal = false;
+
 		for( int i = 0; i < sum.length(); i++ ) {
 			char c = sum.charAt(i);
 			if(Character.isDigit(c)) {
+				
+				
 				if(isDecimal) {
+					// take the new digit and divide by 10 to turn it into decimal
 					result = result + (double) ( c - '0' )/10;
 				} else {
+					// multiply result by 10 before adding the new digit
 					result = result * 10 + (double) ( c - '0' );
 				}
 
 			} else if( c == '.') {
 				isDecimal = true;
 			} else {
+				/* only reset this flag if we encounter space, parenthesis or maths operation as
+				as we might have 1.999999... 
+				*/
 				isDecimal = false;
 			}
 			
+			//encounter open bracket
 			if ( c == '(' && i != 0 ) {
 				String newSum = "";
+
+				// record previous mathematics operation before this numbeer
 				char prevOp = sum.charAt(i - 2);
 				if( prevOp == '+' || prevOp =='-' || prevOp  =='*' || prevOp == '/' ) {
 					sign = prevOp;
 				}
+				
+				// discard the bracket
 				i++;
+
+				// take everything within the parenthesis ( .... )
 				while(i < sum.length() && sum.charAt(i) != ')')
 				{
 					newSum += sum.charAt(i);
@@ -46,7 +64,11 @@ public class Calculator {
 				
 			}
 			
+			// add the result into the stack 
 			if(  i + 1 == sum.length()  || c == '+' || c =='-' || c =='*' || c == '/' ) {
+
+				// do not need to add the result into the stack if the last character is ')' 
+				// as this has been added during the previous call
 				if(sum.charAt(i) != ')')
 				{
 					helper(result, results, sign);
@@ -59,6 +81,7 @@ public class Calculator {
 			
 		}
 		
+		// sum everything in the stack to get the final sum 
 		double finalresult = 0.0;
 		while(!results.isEmpty()) {
 			double cur = results.pop();
@@ -92,7 +115,7 @@ public class Calculator {
 		String equation = input.nextLine();
 		double result = calculate(equation);
 		System.out.println("Total is " + result);
-		
+		input.close();
 	}
 
 }
